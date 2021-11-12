4477b14fe01dbd9da167c41ba15090a9c65b3ea0
Kamarul917 <2002773@sit.singaporetech.edu.sg>
2021-11-12 10:34:53 +0800

    Update process_index.php

--- process_index.php
+++ process_index.php
@@ -1,4 +1,6 @@
 <?php
+
+include 'config.php';
 //// Define and initialize global variables:
 $country = $iso = $errorMsg = "";
 $success = true;
@@ -14,6 +16,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
+    $stmt = $conn->prepare("SELECT * FROM country WHERE iso_code = ?");
+    $stmt->bind_param("s", $iso);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+            // Note that email field is unique, so should only have
+            // one row in the result set.
+            $row = $result->fetch_assoc();
+            $cName = $row["country_name"];
+            $cPop = $row["population"];
+            $stmt->close();
+            $conn->close();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,22 +52,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class='card-body'>
                     <form action='process_function.php' method='post'>
                         <div class='form-group'>
-                            <a><?php echo $iso. " "?> is safe/unsafe.</a><br>
+                            <a><?php echo $cName. " "?> is safe/unsafe.</a><br>
                                 <label for="function1" class='align-self-center'>Select a Function:</label>
                                 <select id='function1' name='function1' class='form-control' aria-label='function1'>
-                                    <option value="1">Function 1</option>
-                                    <option value="2">Function 2</option>
-                                    <option value="3">Function 3</option>
-                                    <option value="4">Function 4</option>
+                                    <option value="1">View Information</option>
+                                    <option value="2">View Graph</option>
                                 </select>
-                                <select id='function2' name='function2' class='form-control' aria-label='function2'>
-                                    <option value="5">Function 5</option>
-                                    <option value="6">Function 6</option>
-                                    <option value="7">Function 7</option>
-                                    <option value="8">Function 8</option>
+                                <select id='function2' name='function2' class='form-control' aria-label='function2' style="margin-top: 80px">
+                                    <option value="3">Daily Vaccination Graph</option>
+                                    <option value="4">Active Case Graph</option>
+                                    <option value="5">Serious Cases and Death Graph</option>
+                                    <option value="6">All Information</option>
+                                    <option value="7">Vaccination %</option>
+                                    <option value="8">Active Cases to Total Population</option>
+                                    <option value="9">Serious Cases to Active Cases</option>
+                                    <option value="10">Vaccines Offered</option>
                                 </select>
                         </div>
-                        <div class="form-group" style="text-align: center; margin-top: 80px;">
+                        <div class="form-group" style="text-align: center; margin-top: 10px;">
                             <button type="submit" value="submit" class="btn btn-primary btn-block text-uppercase account-btn">Let's Go</button>
                         </div> <!-- form-group// -->
                     </form>
